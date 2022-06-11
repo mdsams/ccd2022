ac8f57f7ada6fd515fd1c7b200773e94d3af1e86
madiha123-design <70858557+madiha123-design@users.noreply.github.com>
2022-06-11 16:07:03 +0530

    feat/team-section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import Team from './Components/Teams/Teams'
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Cloud Community Days</h1>
+      {/* <h1 className="text-3xl font-bold text-center py-3">
+        Cloud Community Days
+      </h1> */}
+      <Team />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
