35807cb9aba7051e6279850fec75949f8047821f
mdsams <tabrejmdsams786@gmail.com>
2022-06-13 04:30:20 +0530

    Made gdg loading Screen

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Loading from './Components/Loading';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const footer = ReactDOM.createRoot(document.getElementById('footer') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
+footer.render(
+    <React.StrictMode>
+        <Loading />
+    </React.StrictMode>,
 );
