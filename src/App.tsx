9569ef12be307bef5828408739b57f67c494f58d
mdsams <tabrejmdsams786@gmail.com>
2022-06-13 11:29:33 +0530

    worked on comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Speakers from './Components/Speakers/Speakers';
+import Loading from './Components/Loading';
 
 function App() {
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+    }, []);
+
     return (
         <div>
             {/* <h1 className='text-3xl font-bold underline'>Cloud Community Days</h1> */}
-            <Speakers />
+            {loading ? <Loading /> : <Speakers />}
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Loading from './Components/Loading';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-const footer = ReactDOM.createRoot(document.getElementById('footer') as HTMLElement);
+
 root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-);
-footer.render(
-    <React.StrictMode>
-        <Loading />
     </React.StrictMode>,
 );
