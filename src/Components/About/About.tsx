e3b50736d5422a6993fe75fe4c72be1115bc819a
Avinash Ranjan <ranjan.avinash@hotmail.com>
2022-07-14 22:35:19 +0530

    Update About.tsx

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <div className="mt-8">
       <div className="grid sm:grid-cols-1 md:grid-cols-2  place-items-center bg-lightBlue bg-opacity-50">
-        <div className="w-full lg:w-3/4 my-0 mx-auto pl-20">
+        <div className="w-full lg:w-3/4 my-0 mx-auto pl-8 md:pl-20">
           <h3 className="text-gray-800 text-3xl py-5">Hello, CCD’22</h3>
           <p className="text-gray-600 text-xl">
             GDG Cloud Kolkata is bringing to you the largest Google Cloud developer
