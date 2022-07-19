757273967d8144f6935790e079215becdce9a21d
madiha123-design <70858557+madiha123-design@users.noreply.github.com>
2022-07-19 22:34:21 +0530

    feat/Partners link

--- src/Components/Partners/Partners.tsx
+++ src/Components/Partners/Partners.tsx
@@ -1,5 +1,5 @@
 import Title from '../Title/Title'
-import logo from '../../Images/gdsc-logo.png'
+// import logo from '../../Images/gdsc-logo.png'
 import nseclogo from '../../Images/gdsc-nsec-logo.png'
 import ciemlogo from '../../Images/gdsc-ciem-logo.png'
 import dscsitlogo from '../../Images/gdsc-dscsit-logo.png'
@@ -15,43 +15,76 @@ const Partners = () => {
     <div className="w-2/3 my-0 mx-auto">
       <Title heading="Community Partners" />
       <div className="container mx-auto space-y-2 lg:space-y-0 lg:gap-2 lg:grid lg:grid-cols-3">
-        <div className="w-full rounded">
-            <img src={nseclogo}
-                alt="image"></img>
+        <div
+          className="w-full rounded cursor-pointer"
+          onClick={() => window.open('https://dscnsec.com/', '_blank')}
+        >
+          <img src={nseclogo} alt="logo" />
         </div>
-        <div className="w-full rounded">
-            <img src={ciemlogo}
-                alt="image"></img>
+        <div
+          className="w-full rounded cursor-pointer"
+          onClick={() => window.open('https://dscciem.github.io/', '_blank')}
+        >
+          <img src={ciemlogo} alt="logo" />
         </div>
-        <div className="w-full rounded">
-            <img src={sitlogo}
-                alt="image"></img>
+        <div
+          className="w-full rounded cursor-pointer"
+          onClick={() => window.open('https://gdscsit2021.github.io/', '_blank')}
+        >
+          <img src={sitlogo} alt="logo" />
         </div>
-        <div className="w-full rounded">
-            <img src={gcelogo}
-                alt="image"></img>
+        <div
+          className="w-full rounded cursor-pointer"
+          onClick={() => window.open('https://twitter.com/DSC_GCE', '_blank')}
+        >
+          <img src={gcelogo} alt="logo"/>
         </div>
-        <div className="w-full rounded">
-            <img src={gecbsplogo}
-                alt="image"></img>
+        <div
+          className="w-full rounded cursor-pointer"
+          onClick={() =>
+            window.open(
+              'https://gdsc.community.dev/events/details/developer-student-clubs-government-engineering-college-bilaspur-presents-intro-to-kaggle-gdsc-gecsp/',
+              '_blank'
+            )
+          }
+        >
+          <img src={gecbsplogo} alt="logo"/>
         </div>
-        <div className="w-full rounded">
-            <img src={hitlogo}
-                alt="image"></img>
+        <div
+          className="w-full rounded cursor-pointer"
+          onClick={() => window.open('https://dschit.com/', '_blank')}
+        >
+          <img src={hitlogo} alt="logo" />
         </div>
-        <div className="w-full rounded">
-            <img src={jiscelogo}
-                alt="image"></img>
+        <div
+          className="w-full rounded cursor-pointer"
+          onClick={() =>
+            window.open(
+              'https://www.linkedin.com/in/gdsc-jisce/?trk=public_profile_browsemap&originalSubdomain=in',
+              '_blank'
+            )
+          }
+        >
+          <img src={jiscelogo} alt="logo"/>
         </div>
-        <div className="w-full rounded">
-            <img src={dscsitlogo}
-                alt="image"></img>
+        <div
+          className="w-full rounded cursor-pointer"
+          onClick={() =>
+            window.open(
+              'https://gdsc.community.dev/dr-sudhir-chandra-sur-institute-of-technology-sports-complex-kolkata/',
+              '_blank'
+            )
+          }
+        >
+          <img src={dscsitlogo} alt="logo"/>
         </div>
-        <div className="w-full rounded">
-            <img src={msitlogo}
-                alt="image"></img>
+        <div
+          className="w-full rounded cursor-pointer"
+          onClick={() => window.open('https://dscmsit.github.io/', '_blank')}
+        >
+          <img src={msitlogo} alt="logo"/>
         </div>
-    </div>
+      </div>
     </div>
   )
 }
